a62ab67e674b2c3c485679f64929ded379fffe5b
dasom <dasom589@gmail.com>
2024-02-20 11:03:34 +0900

    2024-02-20

---
+++ 2024.02.19.sql
@@ -0,0 +1,34 @@
+-- *** root 계정으로 실행 *** --
+-- phone 계정 만들기
+create user 'phone'@'%' identified by 'phone';
+
+-- phone 권한 부여
+grant all privileges on phone_db.* to 'phone'@'%';
+
+flush privileges;
+
+-- *** phone 계정으로 실행 *** --
+-- 데이터베이스 생성
+create database phone_db
+	default character set utf8mb4
+    collate utf8mb4_general_ci
+    default encryption='n'
+;
+
+-- 테이블 생성
+create table person (
+	person_id integer auto_increment primary key,
+    name varchar(30),
+    hp varchar(20),
+    company varchar(20)
+);
+
+insert into person
+values (null, '고다솜', '010-9909-9029', '메이아이');
+
+select  person_id,
+		name,
+        hp,
+        company
+from person
+;
